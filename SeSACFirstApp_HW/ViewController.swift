3bb2b04b20c7b25dd84250d00934421cc6bd5d38
jieun <jieun_0330@naver.com>
2023-12-26 21:23:52 +0900

    first commit

---
+++ SeSACFirstApp_HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACFirstApp_HW
+//
+//  Created by 박지은 on 12/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
