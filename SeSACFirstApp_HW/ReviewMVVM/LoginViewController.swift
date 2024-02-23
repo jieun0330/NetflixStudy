3ffa6b0af2caeaf6baa189ef540f746a03f23b69
jieun <jieun_0330@naver.com>
2024-02-23 18:38:05 +0900

    :lipstick: [style] MVVM연습-stackView로 변경

--- SeSACFirstApp_HW/ReviewMVVM/LoginViewController.swift
+++ SeSACFirstApp_HW/ReviewMVVM/LoginViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 import SnapKit
 
 class LoginViewController: BaseViewController {
+    
+    let stackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 20
+        $0.alignment = .center
+        $0.distribution = .equalSpacing
+//        $0.layer.borderColor = UIColor.red.cgColor
+//        $0.layer.borderWidth = 1
+    }
         
     let emailTextField = UITextField().then { _ in
         
@@ -17,6 +26,11 @@ class LoginViewController: BaseViewController {
     let pwTextField = UITextField().then { _ in
         
     }
+    
+    let resultLabel = UILabel().then {
+        $0.text = "결과 라벨 공간"
+        $0.textAlignment = .center
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,23 +38,43 @@ class LoginViewController: BaseViewController {
     }
     
     override func configureHierarchy() {
-        [emailTextField, pwTextField].forEach {
+        [stackView, resultLabel].forEach {
             view.addSubview($0)
         }
+        
+        [emailTextField, pwTextField].forEach {
+            stackView.addSubview($0)
+        }
+        
     }
     
     override func configureConstraints() {
+        
+        stackView.snp.makeConstraints {
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(40)
+            $0.verticalEdges.equalTo(view.safeAreaLayoutGuide).inset(200)
+            $0.centerX.equalToSuperview()
+        }
+        
         emailTextField.snp.makeConstraints {
-            $0.horizontalEdges.equalToSuperview().inset(40)
-            $0.centerY.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.top.equalToSuperview()
             $0.height.equalTo(30)
         }
         
         pwTextField.snp.makeConstraints {
-            $0.horizontalEdges.equalToSuperview().inset(40)
+            $0.horizontalEdges.equalToSuperview()
             $0.top.equalTo(emailTextField.snp.bottom).offset(20)
             $0.height.equalTo(30)
         }
+        
+        resultLabel.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview().inset(40)
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(stackView.snp.bottom)
+            $0.height.equalTo(30)
+        }
+        
     }
     
     override func configureView() {
---
+++ SeSACFirstApp_HW/ReviewMVVM/Observable.swift
@@ -0,0 +1,14 @@
+//
+//  Observable.swift
+//  SeSACFirstApp_HW
+//
+//  Created by 박지은 on 2/23/24.
+//
+
+import Foundation
+
+class Observable {
+    
+    
+    
+}
